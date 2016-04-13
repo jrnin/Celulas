6c7d727f7b6e5bc6fb29c4c60bd2881a14f7f030
jrnin <jrnin@hotmail.com>
2016-04-13 07:20:29 -0300

    Inicio

---
+++ src/main/java/br/com/jrnin/celulas/util/DAOFactory.java
@@ -0,0 +1,10 @@
+package br.com.jrnin.celulas.util;
+import br.com.jrnin.celulas.usuario.*;
+public class DAOFactory {
+
+	public static UsuarioDAO criarUsuarioDAO() {
+		UsuarioDAOHibernate usuarioDAO = new UsuarioDAOHibernate();
+		usuarioDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return usuarioDAO; 
+	}
+}
